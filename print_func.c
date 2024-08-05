26ea19a2531e0f0e3e1d5d23d3b3f31a5a05c5e2
Omnilutiondev <ike.john.agu@gmail.com>
2024-08-05 04:25:34 +0100

    a function that produces output according to a format

--- print_func.c
+++ print_func.c
@@ -10,7 +10,7 @@ void print_buff(char buffer[], int *buff_indx);
 int _printf(const char *format, ...)
 {
 	int idx, prntd = 0, prntd_chars = 0;
-	int flags, width, precision, size, buff_indx = 0;
+	int flg, wdt, preci, sz, buff_indx = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -38,7 +38,7 @@ int _printf(const char *format, ...)
 			size = get_sz(format, &idx);
 			++idx;
 			prntd = hndl_prnt(format, &idx, list, buffer,
-				flags, width, precision, size);
+				flg, wdt, preci, sz);
 			if (prntd == -1)
 				return (-1);
 			prntd_chars += prntd;
